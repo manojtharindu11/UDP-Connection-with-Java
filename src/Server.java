75cca15ff744789ea57be57ee61f02b27a8697ca
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-10-13 11:24:35 +0530

    create udp server

---
+++ src/Server.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket(8080);
+
+        byte[] buffer = new byte[256];
+
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        socket.receive(packet); // wait for client to send a packet
+
+        String received = new String(packet.getData(), 0, packet.getLength());
+
+        System.out.println("Received: " + received);
+
+        String response = "Hello from the server!";
+
+        byte[] responseData = response.getBytes();
+
+        DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, packet.getAddress(), packet.getPort());
+
+        socket.send(responsePacket);
+
+        socket.close();
+    }
+}
